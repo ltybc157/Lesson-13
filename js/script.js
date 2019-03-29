169bb852cef2c84f42f47502279c5fb189a11516
ltybc157 <den.ste21013@yandex.ru>
2019-03-29 07:48:34 +0600

    коммит  lesson 13.1

--- js/script.js
+++ js/script.js
@@ -199,45 +199,103 @@ window.addEventListener('DOMContentLoaded', function () {
 
     let slideIndex = 1, // это перемення отвечает за слайд который находится в текущий момент
         slides = document.querySelectorAll('.slider-item'), //взыввем наши слайдеры все
-        prev = document.querySelectorAll('.prev'), //взыввем наши // cтрелочку крторая двигает слайдер назад
-        next = document.querySelectorAll('.next'), //взыввем наши // cтрелочку крторая двигает слайдер вперед
-        dotsWrap = document.querySelectorAll('.slider-dots'), //получаем обертку наших точек
+        prev = document.querySelector('.prev'), //взыввем наши // cтрелочку крторая двигает слайдер назад
+        next = document.querySelector('.next'), //взыввем наши // cтрелочку крторая двигает слайдер вперед
+        dotsWrap = document.querySelector('.slider-dots'), //получаем обертку наших точек
         dots = document.querySelectorAll('.dot'); //получаем все точки которые будут использоваться
-        
-        showSlides(slideIndex);
+
+    showSlides(slideIndex);
 
     function showSlides(n) { //это функция будет принимать один аргумент для того чобы она переключала слайды
 
-        if (n >slides.length) {// пишим условие для слайдов
-            slideIndex = 1;//установливвем слайдер на 1
+        if (n > slides.length) { // пишим условие для слайдов
+            slideIndex = 1; //установливвем слайдер на 1
         }
 
-        if (n < 1) {// пишим обратное условие
-            slideIndex = slides.length;// если мы нажимаем стрелку назад на 1 слайде показывается последний слайд
+        if (n < 1) { // пишим обратное условие
+            slideIndex = slides.length; // если мы нажимаем стрелку назад на 1 слайде показывается последний слайд
         }
         slides.forEach((item) => item.style.display = 'none'); //1 способ каждому слайдеру передаём убрать дисплей
         //закомментировали после 10:50 for (let i = 0; i < clides.length; i++) { //2 способ каждому слайдеру передаём убрать дисплей
         //slides[i].style.display = 'none'; //2 способ каждому слайдеру передаём убрать дисплей
         //}
-   
-    dots.forEach((item) => item.classList.remove('dot-active')); // уберает классы dot-active  с точек
+
+        dots.forEach((item) => item.classList.remove('dot-active')); // уберает классы dot-active  с точек
+
+        slides[slideIndex - 1].style.display = 'block'; //показываем такой записью 1 слайд
+        dots[slideIndex - 1].classList.add('dot-active'); // показываем такой записью 1 точку
+    }
+
+    function plusSlides(n) { //это функция меняет slideIndex для чтобы менять картинки в слайде
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlaide(n) { //это функция прописывается чтобы узнать какой сейчас слайд стоит
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function () { //это пишется для кнопки перемещение назад
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function () { //это пишется для кнопки перемещение вперед
+        plusSlides(1);
+    });
+    dotsWrap.addEventListener('click', function(event) {//эту функция для точек  в слайдах
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlaide(i);
+            }
+        }
+    });
+ //Calc
+
+
+ let persons = document.querySelectorAll('.counter-block-input')[0],
+    restDays = document.querySelectorAll('.counter-block-input')[1],
+    place = document.getElementById('select'),
+    totalValue = document.getElementById('total'),//вызываем  где общая сумма
+    personsSum = 0,
+    daysSum = 0,
+    total = 0;
     
-    slides[slideIndex - 1].style.display = 'block'; //показываем такой записью 1 слайд
-    dots[slideIndex - 1].classList.add('dot-active');// показываем такой записью 1 точку
- }
+    
+    
+    totalValue.innerHTML = 0;// выводит 0 в общая сумма
 
-function plusSlides(n) {//это функция меняет slideIndex для чтобы менять картинки в слайде
-    showSlides(slideIndex += n);
+    persons.addEventListener('change', function() {//вешаем обработчик событий на графу где водим количество людей
+        personsSum = +this.value;// берёт переменную каторую водил пользователь 
+        total = (daysSum + personsSum)*4000;
+
+        if(restDays.value == '' || persons.value == '') {//пишем условие что второе поле пустое
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+
+    });
+
+    restDays.addEventListener('change', function() {//вешаем обработчик событий на графу где водим количество людей
+        daysSum = +this.value;// берёт переменную каторую водил пользователь 
+        total = (daysSum + personsSum)*4000;
+
+        if(restDays.value == '' || persons.value == '') {//пишем условие что второе поле пустое
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+
+    });
+
+    place.addEventListener('change', function() {
+if (restDays.value == '' || persons.value == '') {
+    totalValue.innerHTML = 0;
+}else {
+    let a = total;
+    totalValue.innerHTML = a * this.options[this.selectedIndex].value;
 }
- function currentSlaide(n) { //это функция прописывается чтобы узнать какой сейчас слайд стоит
-    showSlides(slideIndex = n);
-}
+    });
 
- prev.addEventListener('click', function() {//это пишется для кнопки перемещение назад
-plusSlides(-1); 
-});
 
-next.addEventListener('click', function() {//это пишется для кнопки перемещение вперед
-  plusSlides(1);
-})
+
 });
